1bb8436b9cc946120ea48a0c7620cd6b25068522
Xai Hang <xaihang12@gmail.com>
2022-12-28 14:32:30 -0600

    fixed bugs and added comments

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -7,18 +7,17 @@ console.log('--- A. basket global variable ---');
 const basket = [];
 console.log('Items in basket:', basket); // console log to see if array is empty and it is
 
-
 console.log('--- B. function addItem() ---');
 // - Create a function called `addItem`. It should:
 //   - take an input parameter for a string `item`
 //   - add the new item to the global array `basket`.
 //   - return `true` indicating the item was added
-//***COMMENTS */
-//  created a function named addItem passing through parameter item (string)
-//  any item passing through the function will be added to the global variable, basket with push() method
-//  **push() method to add item to the end of array
-//  and if the parameter item (string) is added successfully to end of basket's array
-//  then we want the return to be true (boolean)
+//***COMMENTS:
+//      declared a function named addItem passing through parameter item (string)
+//      any item passing through the function will be added to the global variable, basket with push() method
+//      **push() method to add item to the end of array
+//      and if the parameter item (string) is added successfully to end of basket's array
+//      then we want the return to be true (boolean)
 function addItem(item) {
   basket.push(item);
   return true;
@@ -28,18 +27,17 @@ console.log('Is cherry added to basket?', addItem('cherry'));
 console.log('Is mango added to basket?', addItem('mango'));
 console.log('Is kiwi added to basket?', addItem('kiwi'));
 console.log('Is pear added to basket?', addItem('pear'));
-console.log('Items in basket:', basket);
-
+console.log('Items in basket:', basket); // current list of items in basket
 
 console.log('--- C. function listItem()  ---');
 // - Create a function called `listItems`. It should:
 //   - loop over the items in the `basket` array
 //   - console.log each individual item on a new line
-//***COMMENTS */
-//  created a function named listItems
-//  inside function added a for loop to loop through each of the items in the basket
-//  if the item is equal to type/value in the basket's index
-//  then return back to us what that item is
+//***COMMENTS:
+//      declared a function named listItems
+//      inside function added a for loop to loop through each of the items in the basket
+//      if the item is equal to type/value in the basket's index
+//      then return back to us what that item is
 function listItems(item) {
   for (let i = 0; i < basket.length; i++) {
     if (item === basket[i]) {
@@ -57,17 +55,17 @@ console.log('single item:', listItems('grape')); //undefine because we have not 
 console.log('--- D. function empty() ---');
 // - Create a function called `empty`. It should:
 //   - reset the `basket` to an empty array
-//***COMMENTS */
-// created a function called empty
-// inside block scope reset the basket by assigning a new length of 0 - this resets the array or empty it
-// to check if the array is truly empty out use a conditional statement comparing the new length to 0
-// is the new length type/value equal to 0? if that is true then return true (boolean);
-// otherwise it will return false (boolean)
-// **note: Array indexes start counting at 0 – we cannot use indexes to check if an array is occupied.
+//***COMMENTS:
+//      declared a function called empty
+//      inside block scope reset the basket by assigning a new length of 0 - this resets the array or empty it
+//      to check if the array is truly empty out use a conditional statement comparing the new length to 0
+//      is the new length type/value equal to 0? if that is true then return true (boolean);
+//      otherwise it will return false (boolean)
+//      **note: Array indexes start counting at 0 – we cannot use indexes to check if an array is occupied.
 //          Therefore, you must check the array length.
 function empty() {
   basket.length = 0; //resets the array; 0 items in basket
-//  basket.length = 2; //sample: shows that there are 2 items in the basket -- this will return false (boolean)
+  //  basket.length = 2; //sample: shows that there are 2 items in the basket -- this will return false (boolean)
   if (basket.length === 0) {
     return true;
   } else {
@@ -75,8 +73,7 @@ function empty() {
   }
 }
 console.log('Is basket empty?', empty()); //should be true, as we empty out the array by assigning the length to 0; otherwise false
-console.log('Numbers of items in basket:', basket.length); 
-
+console.log('Numbers of items in basket:', basket.length); // 0
 
 // ************************************************************* //
 // -------------------- STRETCH GOALS ------------------------- //
@@ -87,50 +84,46 @@ console.log('--- 1. STRETCH GOAL: maxItems global variable ---');
 const maxItems = 5;
 console.log('The maximum number of items in basket:', maxItems); // 5
 
-
 console.log('--- 2. STRETCH GOAL: function isFull() ---');
 //2. Create a function called isFull(). It should:
 // - return `false` if the basket contains *less* than max number of items
 // - return `true` otherwise (equal or more than maxItems)
-//***COMMENTS: */
-//  created isFull function when invoked
-//  we are testing to see if the number of items inside basket is less than maxItems
-//  if yes return false (boolean) otherwise return true (boolean)
+//***COMMENTS: 
+//      declared a function named isFull
+//      we are testing to see if the number of items inside basket is less than maxItems
+//      if yes return false (boolean) otherwise return true (boolean)
 function isFull() {
   if (basket.length < maxItems) {
     return false;
-  } else if (basket.length >= maxItems){
+  } else if (basket.length >= maxItems) {
     return true;
   }
 }
 console.log('numbers of items in basket:', basket.length); // currently 'empty' = 0
 console.log('Is basket full?', isFull()); // should be false
 
-
 console.log('--- 3. STRETCH GOAL: Update the required `addItem` function ---');
 // 3. Update the required `addItem` function to:
 //   - Use the `isFull` function to prevent more than `maxItems` from being added to the basket.
 //   - If an item was added to the array, return `true`
 //   - If there was no room and the item could not be added return `false`
-//***COMMENTS: */
-//
-//
-    //  psuedo coding
-    //  isFull have maxItems (5)? 
-    //  if not than return to me: addItem to basket
-    //  if it is than return to me 'false' as basket is full!
+//***COMMENTS:
+//      created function name updatedItems passing item (string) in parameter
+//      when updatedItem is invoked we will be testing the following conditions
+//      first we will check if the basket is full using isFull() and this must be false 
+//      AND isFull() must be less than maxItems (meaning that there's room in the basket for more)
+//      if both those condition are true then in return use the addItem() - add that item to basket
+//      otherwise, we want to return false as there's no room - basket is full
 
 function updatedItems(item) {
-    
-if (isFull() == false && isFull() < maxItems ) {
+  if (isFull() == false && isFull() < maxItems) {
     return addItem(item);
-} else {
+  } else {
     return false;
+  }
 }
 
-}
-
-console.log('Is basket full?', isFull()); // false 
+console.log('Is basket full?', isFull()); // false
 console.log('Is ginger added to basket?', updatedItems('ginger')); // true
 console.log('Is chive added to basket?', updatedItems('chive')); //true
 console.log('Is onion added to basket?', updatedItems('onion')); // true
@@ -139,7 +132,6 @@ console.log('Is rosemary added to basket?', updatedItems('rosemary')); // true
 console.log('Is tumeric added to basket?', updatedItems('tumeric')); // false - only 5 items max!
 console.log('Is cinnamon added to basket?', updatedItems('cinnamon')); // false - only 5 items max!
 console.log('List of items in basket:', basket); // should be ['ginger', 'chive', 'onion', 'mint', 'rosemary']
-
 
 console.log('--- 4. STRETCH GOAL: function removeItem() ---');
 // __________Using Array built-in functions!__________  //
@@ -151,8 +143,8 @@ console.log('--- 4. STRETCH GOAL: function removeItem() ---');
 
 //***COMMENTS */
 // created a function called removedItem passing through item (string) parameter
-//  in the block scope declared a new variable indexItem and assign it to take the indexOf item that is passing through from basket 
-//  then we test it through conditional statement if the return value of item is greater than -1; 
+//  in the block scope declared a new variable indexItem and assign it to take the indexOf item that is passing through from basket
+//  then we test it through conditional statement if the return value of item is greater than -1;
 //  if it is then in return we want to execute the splice method in the basket based on the return value of indexItem
 //  however if the indexItem value returned -1 (NOT FOUND) then return null
 //  Array.indexOf ==> method returns the first index of a value
@@ -162,15 +154,15 @@ console.log('--- 4. STRETCH GOAL: function removeItem() ---');
 //                   this method overwrites the original array
 
 function removeItem(item) {
-    const indexItem = basket.indexOf(item);
+  const indexItem = basket.indexOf(item);
 
-    if (indexItem > -1) {
-        return basket.splice(indexItem, 1);
-    } else {
-        return null;
-    }
-} 
-console.log('Before - list of items:', basket); //list before item are removed 
+  if (indexItem > -1) {
+    return basket.splice(indexItem, 1);
+  } else {
+    return null;
+  }
+}
+console.log('Before - list of items:', basket); //list before item are removed
 console.log('Item removed:', removeItem('ginger')); // remove ginger from array
 console.log('Item removed:', removeItem('onion')); // remove onion from array
 console.log('Item removed:', removeItem('jello')); // null - because this item is not in the basket to begin with
